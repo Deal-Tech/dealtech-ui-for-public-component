c3a1751e2abc37e13d3e94ef849ba0357891d9c2
M Rizky | Kyy <111228799+mrizky-fr@users.noreply.github.com>
2026-09-21 17:01:03 +0700

    add pricelist section v1

---
+++ sections/PriceList/PricelistV1/PricelistV1.tsx
@@ -0,0 +1,20 @@
+import PriceCardV1 from '../../../elements/Card/PriceCardV1/PriceCardV1';
+
+import './pricelist-v1.css';
+
+export default function PricelistV1() {
+    return (
+        <section id="harga" className="pricelist-v1">
+            <div className="pricelist-v1__shell">
+                <h2 className="pricelist-v1__heading">
+                    Pilih Akses Sesuai{' '}
+                    <span className="pricelist-v1__accent">Kebutuhan Proyek Anda</span>
+                </h2>
+                <p className="pricelist-v1__subtitle">
+                    Pilih koleksi Dealtech UI yang sesuai dan tingkatkan akses kapan saja saat proyek berkembang.
+                </p>
+                <PriceCardV1 />
+            </div>
+        </section>
+    );
+}
