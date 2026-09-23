bcc07ff88f59fbd9d56d5c074792a4f052c8efea
M Rizky | Kyy <111228799+mrizky-fr@users.noreply.github.com>
2026-09-23 12:51:49 +0700

    fix dynamic hero curves

--- sections/Hero/SectionV11/SectionV11.tsx
+++ sections/Hero/SectionV11/SectionV11.tsx
@@ -86,7 +86,18 @@ export default function SectionV11() {
                     </div>
                 </div>
             </div>
-
+            <svg
+                className="hero-v11__shape"
+                viewBox="0 0 1440 100"
+                preserveAspectRatio="none"
+                aria-hidden="true"
+            >
+                <path
+                    className="hero-v11__shape-shadow"
+                    d="M0 3H530C620 3 625 65 720 65C815 65 820 3 910 3H1440V100H0Z"
+                />
+                <path d="M0 12H530C620 12 625 74 720 74C815 74 820 12 910 12H1440V100H0Z" />
+            </svg>
         </section>
     );
 }
